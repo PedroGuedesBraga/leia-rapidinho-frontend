1004f1ec4e84ec8a3c4e0fb499a5c4b85495f7a6
Pedro Guedes <pedro.braga@ccc.ufcg.edu.br>
2021-07-13 01:22:43 -0300

    Cria tela de login responsiva para desktop

--- src/containers/register/index.js
+++ src/containers/register/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles.css';
 import Logo from '../../components/logo';
-import { Button } from 'semantic-ui-react';
+import { Button, Form, Input } from 'semantic-ui-react';
 
 const Register = () => {
 
@@ -9,12 +9,18 @@ const Register = () => {
         <div className="container">
             <div className="msg">
                 <Logo></Logo>
+                <h2 className="logo-subtitle">Já tem uma conta? Clique aqui</h2>
             </div>
-            <div className="form">
-                <div className="form-header">Crie sua conta</div>
-                <div className="form-body">
-                    <Button>Clique aqui</Button>
-                </div>
+            <div className="frm">
+                <h2>Crie sua conta</h2>
+                <Form size="large">
+                    <Form.Input required label='Nome' placeholder='Digite seu nome' width={9} />
+                    <Form.Input required label='Sobrenome' placeholder='Digite seu sobrenome' width={9} />
+                    <Form.Input icon="at" required label='Email' type="email" placeholder='Digite seu email' width={9} />
+                    <Form.Input required label='Senha' type="password" placeholder='Deve conter entre 8 e 32 dígitos, sendo letras e números' width={9} />
+                    <Form.Input required label='Confirmação de senha' type="password" placeholder='Confirme sua senha' width={9} />
+                    <Button className="ui button" color="green" size="large" type="submit">Criar conta</Button>
+                </Form>
             </div>
         </div>
     )
